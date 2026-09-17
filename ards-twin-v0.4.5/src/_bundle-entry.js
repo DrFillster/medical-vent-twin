ceba90bf40e3fbe4c663a41e58738cde667b902e
DrFillster <philipbernard@gmail.com>
2026-09-17 15:19:38 -0500

    feat(v0.5): export Berlin virtual patient APIs to browser bundle

--- ards-twin-v0.4.5/src/_bundle-entry.js
+++ ards-twin-v0.4.5/src/_bundle-entry.js
@@ -1,6 +1,9 @@
 // Entry that re-exports everything.
 export { Simulation, VcAcController, PcAcController } from './simulation.js';
 export { PRESETS } from './presets.js';
+export { BERLIN_COHORT_ENVELOPES, RECRUITABILITY_PRESETS,
+         classifyBerlinOxygenation, makeBerlinVirtualPatient,
+         listBerlinVirtualPatientMatrix } from './clinical_scenarios.js';
 export { makePatientParams, makeInitialState, makeBoundaryFlow,
          makeBoundaryPressure } from './contracts.js';
 export { forwardElasticVolume, dPressureDVolume,
