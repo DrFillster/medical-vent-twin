11e682bfa97f220627fc6d94f54c6e4c531a45bb
DrFillster <philipbernard@gmail.com>
2026-09-17 15:20:47 -0500

    feat(v0.5): expose virtual patients and twin contract in browser engine

--- ards-twin-v0.4.5/src/browser-entry.js
+++ ards-twin-v0.4.5/src/browser-entry.js
@@ -1,6 +1,8 @@
 module.exports = {
   ...require('./simulation.js'),
   ...require('./presets.js'),
+  ...require('./clinical_scenarios.js'),
+  ...require('./digital_twin_contract.js'),
   ...require('./contracts.js'),
   ...require('./scenario.js'),
 };
