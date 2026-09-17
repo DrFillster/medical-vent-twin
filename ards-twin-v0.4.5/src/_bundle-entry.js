48d19229b959d7fcd2e90c2649a48fa6656bde10
DrFillster <philipbernard@gmail.com>
2026-09-17 18:28:27 -0500

    feat: export verified HumMod source clock

--- ards-twin-v0.4.5/src/_bundle-entry.js
+++ ards-twin-v0.4.5/src/_bundle-entry.js
@@ -16,7 +16,7 @@ export { createHumModStandaloneExportMapper } from './hummod_standalone_binding.
 export { HUMMOD_EXPORT_SCHEMA, validateHumModTrajectoryExport,
          normalizeHumModTrajectoryExport,
          createReplayProviderFromHumModExport } from './hummod_export_contract.js';
-export { HUMMOD_RUN_REQUEST_SCHEMA, createHumModRunRequest,
+export { HUMMOD_RUN_REQUEST_SCHEMA, HUMMOD_SOURCE_CLOCK, createHumModRunRequest,
          assertRunnerClockVerified } from './hummod_runner_contract.js';
 export { createBerlinHumModReplayRuntime } from './clinical_twin_runtime.js';
 export { createBerlinClinicalTwinSession, buildController } from './clinical_twin_session.js';
