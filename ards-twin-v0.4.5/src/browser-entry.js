a62d8ead55d920ea41539abae691b249ef505148
DrFillster <philipbernard@gmail.com>
2026-09-17 15:36:39 -0500

    feat: export Berlin case catalog to browser build

--- ards-twin-v0.4.5/src/browser-entry.js
+++ ards-twin-v0.4.5/src/browser-entry.js
@@ -2,6 +2,7 @@ module.exports = {
   ...require('./simulation.js'),
   ...require('./presets.js'),
   ...require('./clinical_scenarios.js'),
+  ...require('./berlin_case_catalog.js'),
   ...require('./digital_twin_contract.js'),
   ...require('./contracts.js'),
   ...require('./scenario.js'),
