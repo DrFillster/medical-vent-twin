c7a2b158a66656e4d567804715cc764f8b7c861f
DrFillster <philipbernard@gmail.com>
2026-09-17 17:57:52 -0500

    feat: export composed clinical twin session

--- ards-twin-v0.4.5/src/browser-entry.js
+++ ards-twin-v0.4.5/src/browser-entry.js
@@ -9,6 +9,7 @@ module.exports = {
   ...require('./hummod_standalone_binding.js'),
   ...require('./hummod_export_contract.js'),
   ...require('./clinical_twin_runtime.js'),
+  ...require('./clinical_twin_session.js'),
   ...require('./bedside_measurements.js'),
   ...require('./contracts.js'),
   ...require('./scenario.js'),
