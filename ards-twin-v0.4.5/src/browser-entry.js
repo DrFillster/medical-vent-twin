0b44fb3f75296c791acd95aace254c0efcc55a8b
DrFillster <philipbernard@gmail.com>
2026-09-17 20:52:04 -0500

    feat: export reference-case calibration profile

--- ards-twin-v0.4.5/src/browser-entry.js
+++ ards-twin-v0.4.5/src/browser-entry.js
@@ -15,6 +15,7 @@ module.exports = {
   ...require('./clinical_twin_session.js'),
   ...require('./clinical_session_record.js'),
   ...require('./clinical_case_readiness.js'),
+  ...require('./reference_case_calibration.js'),
   ...require('./recruitment_history.js'),
   ...require('./bedside_measurements.js'),
   ...require('./contracts.js'),
